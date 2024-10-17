5aff09655061750281fd167a519944234d847929
Ayush varshney <ayushvarshney42003@gmail.com>
2024-10-17 10:33:36 +0530

    updated progress

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [activeTab, setActiveTab] = useState('progress'); // Manage which tab is active
-  const lastUpdated = '16 Oct\'24 12:30 PM'; // You can update this dynamically as well
+  const lastUpdated = '17 Oct\'24 10:00 AM'; // You can update this dynamically as well
 
   const [isModalOpen, setIsModalOpen] = useState(true); // State for modal
 
